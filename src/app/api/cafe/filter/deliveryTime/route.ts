3c0f19aee573d2fdb1f670e48fa828aa6942bc9a
sinhashlok <shlokjp@gmail.com>
2024-06-21 18:57:45 +0530

    logs added

--- src/app/api/cafe/filter/deliveryTime/route.ts
+++ src/app/api/cafe/filter/deliveryTime/route.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
+    console.log(body);
+    
     const { deliveryTimeCode } = body;
 
     let cafe: any = [];
